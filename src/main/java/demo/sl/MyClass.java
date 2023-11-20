7d3779338d2c18209b2f6babc05017290fba87c3
Gsv123 <127535614+Gsv123@users.noreply.github.com>
2023-11-20 21:47:22 +0530

    Update MyClass.java

--- src/main/java/demo/sl/MyClass.java
+++ src/main/java/demo/sl/MyClass.java
@@ -13,15 +13,10 @@ public class MyClass {
 	{
 		return a*b;
 	}
-	public int div(int a, int b)
-	{
-		return a/b;
-	}
 	public static void main(String[] args) {		
 		MyClass ob = new MyClass();
 		System.out.println("Sum is : "+ob.sum(50, 25));
 		System.out.println("Difference is : "+ob.sub(50, 25));
 		System.out.println("Multiply is : "+ob.mul(50, 25));
-		System.out.println("Division is : "+ob.div(50, 25));
 	}
 }
